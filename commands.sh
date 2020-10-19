c4d309ff70f2805c333b8d49cc92653f2f496091
rzayanov <rzayanov@gmail.com>
2020-10-19 14:32:34 +0100

    proper file names

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python qc.py -coarse TRAIN.txt DEV-questions.txt > predicted-labels-coarse.txt
-python qc.py -fine TRAIN.txt DEV-questions.txt > predicted-labels-fine.txt
-python ./evaluate.py DEV-labels.txt predicted-labels-coarse.txt
-python ./evaluate.py DEV-labels.txt predicted-labels-fine.txt
+python qc.py -coarse TRAIN.txt DEV-questions.txt > develop56-coarse.txt
+python qc.py -fine TRAIN.txt DEV-questions.txt > develop56-fine.txt
+python ./evaluate.py DEV-labels.txt develop56-coarse.txt
+python ./evaluate.py DEV-labels.txt develop56-fine.txt
