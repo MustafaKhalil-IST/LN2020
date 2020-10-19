5f6d81cc7d73c8954dfc9a72b6990988e9785ba9
rzayanov <rzayanov@gmail.com>
2020-10-19 15:59:44 +0100

    commands for TEST.txt

--- commands.sh
+++ commands.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 python qc.py -coarse TRAIN.txt DEV-questions.txt > develop56-coarse.txt
-python qc.py -fine TRAIN.txt DEV-questions.txt > develop56-fine.txt
+python qc.py -fine   TRAIN.txt DEV-questions.txt > develop56-fine.txt
 python ./evaluate.py DEV-labels.txt develop56-coarse.txt
 python ./evaluate.py DEV-labels.txt develop56-fine.txt
+
+python qc.py -coarse TRAIN.txt TEST.txt > test56-coarse.txt
+python qc.py -fine   TRAIN.txt TEST.txt > test56-fine.txt
