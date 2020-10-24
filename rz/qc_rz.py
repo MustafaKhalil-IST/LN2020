538739c388113cfa96f047ab5fb319c95653c649
rzayanov <rzayanov@gmail.com>
2020-10-24 21:29:15 +0100

    experiment records

--- rz/qc_rz.py
+++ rz/qc_rz.py
@@ -1,6 +1,6 @@
 import sys
 
-from rz.methods import predict_random, predict_levenshtein
+from rz.methods import predict_random, predict_levenshtein, predict_svm
 
 from rz.common import train_and_predict
 
@@ -9,7 +9,7 @@ coarse = args[1] == '-coarse'
 train_fname = args[2]
 target_q_fname = args[3]
 
-_, predicted_ls = train_and_predict(coarse, train_fname, target_q_fname, ['stop_wh'], predict_levenshtein)
+_, predicted_ls = train_and_predict(coarse, train_fname, target_q_fname, [], predict_svm)
 
 for l in predicted_ls:
     print(l)
